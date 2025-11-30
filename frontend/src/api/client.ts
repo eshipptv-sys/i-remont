9fa64b0af0842bb75ddd5ad42af3278528d94506
eshipptv-sys <eshipptv@gmail.com>
2025-11-30 21:08:38 +0300

    Delete frontend directory

--- frontend/src/api/client.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const baseURL = import.meta.env.VITE_API_BASE || 'http://localhost:4000/api';
-
-export const api = axios.create({
-  baseURL
-});
--- frontend/src/api/orders.ts
+++
@@ -1,7 +0,0 @@
-import { api } from './client';
-import { OrderPayload } from '../types';
-
-export const createOrder = async (payload: OrderPayload) => {
-  const response = await api.post<{ orderId: number; totalAmount: number }>('/orders', payload);
-  return response.data;
-};
