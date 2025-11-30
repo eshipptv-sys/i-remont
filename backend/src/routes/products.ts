e7d547bb92f2ac1a57cb22d7b636a4a97d001019
eshipptv-sys <eshipptv@gmail.com>
2025-11-30 21:08:01 +0300

    Delete backend directory

--- backend/src/routes/products.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getProductBySlug, getProducts } from '../controllers/productsController.js';
-
-const router = Router();
-
-router.get('/', getProducts);
-router.get('/:slug', getProductBySlug);
-
-export default router;
